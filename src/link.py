3c105e02e19b57033d1bf08477f16b1af85a31f2
TechWiz-3 <programmingplus1@gmail.com>
2022-08-04 13:09:59 +1000

    🐛 FIX: bug

--- src/link.py
+++ src/link.py
@@ -4,6 +4,8 @@ from sys import exit
 import concurrent.futures
 import threading
 
+thread_local = threading.local()
+
 def get_session():
     if not hasattr(thread_local, "session"):
         thread_local.session = requests.Session()
